14343c1dfd06f19fdf2919df36fb18516812fcd9
Polina Kudryavtseva <pekudryavtseva@avito.ru>
2023-04-20 17:37:11 +0300

    iter6

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,16 +4,33 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/kontik-pk/yandex-metrics-scraper/internal/flags"
 	"github.com/kontik-pk/yandex-metrics-scraper/internal/handlers"
-	"log"
+	log "github.com/kontik-pk/yandex-metrics-scraper/internal/logger"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	log.SugarLogger = *logger.Sugar()
+
 	params := flags.Init(flags.WithAddr())
 	r := chi.NewRouter()
+	r.Use(log.RequestLogger)
 	r.Post("/update/{type}/{name}/{value}", handlers.SaveMetric)
 	r.Get("/value/{type}/{name}", handlers.GetMetric)
 	r.Get("/", handlers.ShowMetrics)
 
-	log.Fatal(http.ListenAndServe(params.FlagRunAddr, r))
+	log.SugarLogger.Infow(
+		"Starting server",
+		"addr", params.FlagRunAddr,
+	)
+	if err := http.ListenAndServe(params.FlagRunAddr, r); err != nil {
+		// записываем в лог ошибку, если сервер не запустился
+		log.SugarLogger.Fatalw(err.Error(), "event", "start server")
+	}
 }
